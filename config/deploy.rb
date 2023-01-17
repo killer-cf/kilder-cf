c474e94ceb386e3c631920a926b0f3e989c3873e
killer-cf <costa.kilder@live.com>
2023-01-17 17:08:32 -0300

    new config

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :migration_role, :app
 set :log_level, :debug
 set :user, 'deploy'
 set :pty,             true
-#set :use_sudo,        false
+set :use_sudo,        false
 set :stage,           :production
 append :linked_files, "config/database.yml", "config/master.key"
 append :linked_dirs, "storage", "log", "tmp", "public/system"
@@ -18,7 +18,7 @@ set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_access_log, "#{shared_path}/log/puma_access.log"
 set :puma_error_log, "#{shared_path}/log/puma_error.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+#set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
