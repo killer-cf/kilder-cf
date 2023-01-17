0ce1cd757d93a9784b72b19b5e531a84ed6d8fb6
killer-cf <costa.kilder@live.com>
2023-01-17 16:58:33 -0300

    new config

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :format, :airbrussh
 set :migration_role, :app
 set :log_level, :debug
 set :user, 'deploy'
-set :use_sudo,        false
+set :pty,             true
 set :stage,           :production
 append :linked_files, "config/database.yml", "config/master.key"
 append :linked_dirs, "storage", "log", "tmp", "public/system"
