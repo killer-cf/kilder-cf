3cd77971f269fc1b5546e1aa31fd74fe0f99b36d
killer-cf <costa.kilder@live.com>
2023-01-17 16:24:47 -0300

    new config

--- Capfile
+++ Capfile
@@ -1,5 +1,3 @@
-set :pty, true
-
 require "capistrano/setup"
 require "capistrano/deploy"
 require "capistrano/scm/git"
@@ -12,5 +10,6 @@ install_plugin Capistrano::SCM::Git
 install_plugin Capistrano::Puma
 install_plugin Capistrano::Puma::Workers
 install_plugin Capistrano::Puma::Nginx
+install_plugin Capistrano::Puma::Systemd
 
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -7,13 +7,21 @@ set :keep_releases, 5
 set :format, :airbrussh
 set :migration_role, :app
 set :log_level, :debug
-set :use_sudo, true
+set :user, 'deploy'
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
 append :linked_files, "config/database.yml", "config/master.key"
 append :linked_dirs, "storage", "log", "tmp", "public/system"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_access_log, "#{shared_path}/log/puma_access.log"
 set :puma_error_log, "#{shared_path}/log/puma_error.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
  
 set :nginx_sites_available_path, "/etc/nginx/sites-available"
 set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
