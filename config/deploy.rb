cd8f25140b70be9cfcc7c3297558ac44043dd263
killer-cf <costa.kilder@live.com>
2023-01-27 11:00:16 -0300

    new

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,8 +14,7 @@ set :stage, :production
 set :passenger_roles, :app
 set :rvm_ruby_version, '3.1.0'
 set :passenger_environment_variables, {
-  'PASSENGER_INSTANCE_REGISTRY_DIR' => '/tmp',
-  'RAILS_ENV=production'
+  'PASSENGER_INSTANCE_REGISTRY_DIR' => '/tmp'
 }
 #set :passenger_restart_command, '/etc/init.d/passenger-kilder-cf.sh restart'
 #set :passenger_restart_options, -> { "kilder-cf 3000" }
